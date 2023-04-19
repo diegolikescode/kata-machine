e38f9093c733222b4bfb685fa7a19e50f3f7fe7d
diegolikescode <diegoprestes.git@hotmail.com>
2023-04-19 18:03:27 -0300

    bubble sort but nicer

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,16 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-	let sorted = false;
-	let sortedValsQnt = 0;
-	do {
-		sorted = true;
-		for (let i = 1; i < arr.length - sortedValsQnt; i++) {
-			if (arr[i - 1] > arr[i]) {
-				sorted = false;
-				const aux = arr[i - 1];
-				arr[i - 1] = arr[i];
-				arr[i] = aux;
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const aux = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = aux;
 			}
 		}
-	} while (!sorted);
-	console.log(arr);
+	}
 }
