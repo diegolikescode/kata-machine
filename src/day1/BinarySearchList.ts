c95d0ae406bce1c6bcdce11bb95eb6558e0983d1
diegolikescode <diegoprestes.git@hotmail.com>
2023-04-19 17:02:53 -0300

    binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+	const getMid = (mlo: number, mhi: number) =>
+		Math.floor(mlo + (mhi - mlo) / 2);
+	let lo = 0;
+	let hi = haystack.length;
 
+	do {
+		const m = getMid(lo, hi);
+		const val = haystack[m];
+
+		if (needle === val) {
+			return true;
+		} else if (needle > val) {
+			lo = m + 1;
+		} else {
+			hi = m;
+		}
+	} while (lo < hi);
+	return false;
 }
