62b4c68f02f556c692ff62dd3c2052a976e23430
diegolikescode <diegoprestes.git@hotmail.com>
2023-04-19 17:21:30 -0300

    two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,14 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+	const sqrt = Math.sqrt(breaks.length);
 
+	let srBreak = 0;
+	for (let i = sqrt; i < breaks.length; i += sqrt) {
+		if (breaks[i]) srBreak = i;
+		break;
+	}
+
+	for (let i = srBreak - sqrt; i < breaks.length; i++) {
+		if (breaks[i]) return i;
+	}
+	return -1;
 }
