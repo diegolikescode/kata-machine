0b19ac5749a6047a9b9a7f819ae73b102bc3668b
diegolikescode <diegoprestes.git@hotmail.com>
2023-04-19 17:55:42 -0300

    bubble sort but its dumb

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
 export default function bubble_sort(arr: number[]): void {
-
+	let sorted = false;
+	let sortedValsQnt = 0;
+	do {
+		sorted = true;
+		for (let i = 1; i < arr.length - sortedValsQnt; i++) {
+			if (arr[i - 1] > arr[i]) {
+				sorted = false;
+				const aux = arr[i - 1];
+				arr[i - 1] = arr[i];
+				arr[i] = aux;
+			}
+		}
+	} while (!sorted);
+	console.log(arr);
 }
