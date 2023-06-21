3cec57543287e03280099695c3addcc9e9635d0b
diegolikescode <diegoprestes.git@hotmail.com>
2023-06-21 20:40:56 -0300

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,39 @@
+type Node<T> = {
+	value: T;
+	previous?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
+	public length: number;
+	private head?: Node<T>;
 
-    
+	constructor() {
+		this.length = 0;
+		this.head = undefined;
+	}
 
-    constructor() {
-    }
+	push(item: T): void {
+		const node = { value: item } as Node<T>;
+		this.length++;
+		if (!this.head) {
+			this.head = node;
+			return;
+		}
+		node.previous = this.head;
+		this.head = node;
+	}
 
-    push(item: T): void {
+	pop(): T | undefined {
+		if (this.length > 0) this.length--;
+		if (!this.head) {
+			return undefined;
+		}
+		const head = this.head;
+		this.head = this.head.previous;
+		return head.value;
+	}
 
-}
-    pop(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+	peek(): T | undefined {
+		return this.head?.value;
+	}
 }
